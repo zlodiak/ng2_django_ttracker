6eb6dd2d4e9f9e81dfc648d52a9ef0131681b027
zlodiak <prozaik81-2@yandex.ru>
2017-12-10 13:18:56 +0300

    urls fix

--- app_tasks/urls.py
+++ app_tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^index$', views.index, name='index'),
+    url(r'^all_statuses$', views.all_statuses, name='all_statuses'),
 ]
--- app_tasks/views.py
+++ app_tasks/views.py
@@ -4,9 +4,15 @@ from django.http import JsonResponse
 from django.core import serializers
 import json
 
-from .models import Task
+from .models import Task, Status
 
 def index(request):
   tasks = Task.objects.all()   
   tasks_serialized = serializers.serialize('json', tasks)
   return JsonResponse(tasks_serialized, safe=False) 
+
+
+def all_statuses(request):
+  statuses = Status.objects.all()   
+  statuses_serialized = serializers.serialize('json', statuses)
+  return JsonResponse(statuses_serialized, safe=False)   
